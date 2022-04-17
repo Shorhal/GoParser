b1c1a1a34d1f4f8b0d36ff452862f557da1c92a3
Shorshal <smlkorigov@gmil.com>
2022-04-17 17:17:24 +0300

    Another try

--- GispParser/GispParserApp.go
+++ GispParser/GispParserApp.go
@@ -4,24 +4,34 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/chromedp/cdproto/emulation"
 	"github.com/chromedp/chromedp"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
 )
 
 func main() {
+	var db *gorm.DB
 
-	//connect()
+	dsn := "sqlserver://@localhost:52876?database=Gisp"
+	//Соединение с БД
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("filed connection")
+	} else {
+		fmt.Println("Success connect to MSSQL")
+	}
+
+	//Подготовка
 	ctx, cancel := chromedp.NewContext(
 		context.Background(),
 		chromedp.WithLogf(log.Printf),
 	)
 	defer cancel()
 
-	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	ctx, cancel = context.WithTimeout(ctx, 20*time.Second)
 	defer cancel()
 
 	emulation.SetUserAgentOverride("WebScrapper 1.0")
@@ -29,27 +39,27 @@ func main() {
 	url := "https://gisp.gov.ru/pp719v2/pub/org/"
 
 	var pagesInfo string
-	var pagesCount int
 	var dataGrid string
-	var allDataGrid string
-
+	//var allDataGrid string
+	//Переход на главную страницу
 	if err := chromedp.Run(ctx,
-
 		chromedp.Navigate(url),
 		chromedp.SetAttributeValue(`#datagrid > div > div.dx-datagrid-pager.dx-pager > div.dx-pages > div.dx-info`, "style", "display: block", chromedp.ByID),
 		chromedp.Text(`#datagrid > div > div.dx-datagrid-pager.dx-pager > div.dx-pages > div.dx-info`, &pagesInfo),
-		chromedp.OuterHTML(`#datagrid > div > div.dx-datagrid-rowsview.dx-scrollable.dx-visibility-change-handler.dx-scrollable-both.dx-scrollable-simulated.dx-scrollable-customizable-scrollbars > div > div > div.dx-scrollable-content > div > table > tbody`, &dataGrid, chromedp.ByID),
+		chromedp.OuterHTML(`#datagrid > div > div.dx-datagrid-rowsview.dx-scrollable.dx-visibility-change-handler.dx-scrollable-both.dx-scrollable-simulated.dx-scrollable-customizable-scrollbars > div > div > div.dx-scrollable-content > div > table`, &dataGrid, chromedp.ByID),
 	); err != nil {
 
 		log.Fatal(err)
 	}
 
-	pagesCount, err := strconv.Atoi(strings.Split(pagesInfo, " ")[3])
-	if err == nil {
+	//pagesCount, err := strconv.Atoi(strings.Split(pagesInfo, " ")[3])
+	if err != nil {
 		fmt.Println("Error")
 	}
 
-	for i := 0; i < pagesCount; i++ {
+	//Сбор данных со страниц
+	/*for i := 1; i <= pagesCount; i++ {
+
 		if err := chromedp.Run(ctx,
 			chromedp.OuterHTML(`#datagrid > div > div.dx-datagrid-rowsview.dx-scrollable.dx-visibility-change-handler.dx-scrollable-both.dx-scrollable-simulated.dx-scrollable-customizable-scrollbars > div > div > div.dx-scrollable-content > div > table`, &dataGrid, chromedp.ByID),
 		); err != nil {
@@ -57,9 +67,12 @@ func main() {
 			log.Fatal(err)
 		}
 		allDataGrid += dataGrid
-	}
-	//fmt.Println(allDataGrid)
-	fmt.Println(pagesCount)
-	fmt.Println(len(parseToOrg(allDataGrid)))
+	}*/
 
+	Org := parseToOrg(dataGrid)
+	//Prod := parseToProd(prodDataGrid)
+
+	for _, element := range Org {
+		db.Create(element)
+	}
 }
